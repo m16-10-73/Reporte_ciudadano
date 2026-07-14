557757e79703604bf0ab1a837612379b07c4de3b
m16-10-73 <manuelcabezasb1673@gmail.com>
2026-07-13 22:35:46 -0400

    Ajuste de puerto para Render

--- app.js
+++ app.js
@@ -221,8 +221,8 @@ app.post('/registrar-incidencia', upload.single('foto'), async (req, res) => {
 
     const cartero = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true, 
+        port: 587,
+        secure: false, 
         auth: {
             user: process.env.EMAIL_USER,    
             pass: process.env.EMAIL_PASS  
