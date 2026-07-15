452ea22f81ff16ed21d2926866abf0a92c20357c
m16-10-73 <manuelcabezasb1673@gmail.com>
2026-07-15 00:06:03 -0400

    corregido puerto y comillas en app listen

--- app.js
+++ app.js
@@ -332,6 +332,6 @@ app.post('/actualizar-estado', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`🚀 Servidor corriendo en el puerto ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor iniciado en el puerto: ${PORT}`);
 });
